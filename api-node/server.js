4e387423e0b49ae8a04ac7aceb932cb6c44ba211
deivideguilherme <deivide.jau@gmail.com>
2024-09-01 20:23:49 -0300

    Instalado o axios para conexão com o frontend, habilitado o cors para a comunicação entre as aplicações, banco de dados configurado com sucesso

--- api-node/server.js
+++ api-node/server.js
@@ -1,60 +1,65 @@
-import express from 'express';
-import { PrismaClient } from '@prisma/client'
+import express from "express";
+import cors from "cors";
+import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
+app.use(cors()); //liberando o acesso ao CORS
 
-const users = []
+const users = [];
 
 //Rota para Listar usuários já criados
-app.get('/usuarios', async (req, res) => {
-    const users = await prisma.user.findMany()    
+app.get("/usuarios", async (req, res) => {
+  const users = await prisma.user.findMany();
 
-    res.status(200).json(users)
-})
+  res.status(200).json(users);
+});
 
 //Rota para Criação de usuários
-app.post('/usuarios', async (req, res) => {
-    const user = await prisma.user.create({
-        data: {
-            email: req.body.email,
-            age: req.body.age,
-            name: req.body.name
-        }
-    })
+app.post("/usuarios", async (req, res) => {
+  const user = await prisma.user.create({
+    data: {
+      email: req.body.email,
+      age: req.body.age,
+      name: req.body.name,
+    },
+  });
 
-    res.status(201).json(user)
-})
-
+  res.status(201).json(user);
+});
 
 //Rota para Atualizar/Editar usuários
-app.put('/usuarios/:id', async (req, res) => {
-    const user = await prisma.user.update({
-        where: {
-            id: req.params.id
-        },
+app.put("/usuarios/:id", async (req, res) => {
+  const user = await prisma.user.update({
+    where: {
+      id: req.params.id,
+    },
 
-        data: {
-            email: req.body.email,
-            age: req.body.age,
-            name: req.body.name
-        }
-    })
+    data: {
+      email: req.body.email,
+      age: req.body.age,
+      name: req.body.name,
+    },
+  });
 
-    res.status(200).json(user)
-})
+  res.status(200).json(user);
+});
 
 //Rota para deletar usuários
-app.delete('/usuarios/:id', async (req, res) => {
-    await prisma.user.delete({
-        where: {
-            id: req.params.id
-        },
-    })
+app.delete("/usuarios/:id", async (req, res) => {
+  await prisma.user.delete({
+    where: {
+      id: req.params.id,
+    },
+  });
 
-    res.status(200).json({ message: 'Usuário deletado com sucesso!' })
-})
+  res.status(200).json({ message: "Usuário deletado com sucesso!" });
+});
 
-app.listen(3000)
+app.listen(3000);
+
+/**
+ * Endereço local da aplicação: https://localhost:3000
+ */
